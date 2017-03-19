9748fd9f37f7dd13cedce42963cdad6fbae2735c
Long Vy. Dang <vylong93khtn@gmail.com>
2017-03-19 17:47:38 +0700

    Testing:add scrolling testing sequence
    Signed-off-by: Long Vy. Dang <vylong93khtn@gmail.com>

--- app/src/main.c
+++ app/src/main.c
@@ -46,7 +46,26 @@
  * 			@arg LCD display project LOGO in one second at dim mode.
  * 			@arg LCD display project LOGO in one second at normal contrast.
  * 			@arg LCD turn off in one second.
- * 			@arg LCD turn on and the project LOGO show back.
+ * 			@arg LCD turn on and the project LOGO show back in one second.
+ * 			@arg LCD invert the pixel in one second.
+ * 			@arg LCD return normal display mode and scroll Left hold screen in one second
+ * 			@arg scroll right hold screen in one second
+ * 			@arg scroll diagleft hold screen in one second
+ * 			@arg scroll diagRight hold screen in one second
+ * 			@arg stop scroll in one second
+ * 			@arg scroll left page 1 and 2 in one second
+ * 			@arg scroll right page 3 in one second
+ * 			@arg scroll diagleft page 2 and 3 in three second
+ * 			@arg scroll diagright page 0 and 1 in three second
+ * 			@arg scroll up 11.428ms interval in three second
+ * 			@arg scroll up 17.143ms interval in three second
+ * 			@arg scroll up 22.857ms interval in three second
+ * 			@arg scroll up 28.571ms interval in three second
+ * 			@arg scroll up 142.857ms interval in three second
+ * 			@arg scroll up 365.714ms interval in three second
+ * 			@arg scroll up 731.429ms interval in three second
+ * 			@arg scroll up 1462.857ms interval in three second
+ * 			@arg stop scroll
  * @retval None
  */
 void test_DisplayDriver(void)
@@ -59,6 +78,51 @@ void test_DisplayDriver(void)
 	display_turnOff();
 	bsp_delay_ms(1000);
 	display_turnOn();
+	bsp_delay_ms(1000);
+	display_setInvertMode();
+	bsp_delay_ms(1000);
+	display_setNormalMode();
+	display_scrollLeft(DISP_PAGE_0, DISP_PAGE_3);
+	bsp_delay_ms(1000);
+	display_scrollRight(DISP_PAGE_0, DISP_PAGE_3);
+	bsp_delay_ms(1000);
+	display_scrollDiagLeft(DISP_PAGE_0, DISP_PAGE_3);
+	bsp_delay_ms(1000);
+	display_scrollDiagRight(DISP_PAGE_0, DISP_PAGE_3);
+	bsp_delay_ms(1000);
+	display_scrollStop();
+	bsp_delay_ms(1000);
+	display_render_logo();
+	display_scrollLeft(DISP_PAGE_1, DISP_PAGE_2);
+	bsp_delay_ms(1000);
+	display_render_logo();
+	display_scrollRight(DISP_PAGE_3, DISP_PAGE_3);
+	bsp_delay_ms(1000);
+	display_render_logo();
+	display_scrollDiagLeft(DISP_PAGE_2, DISP_PAGE_3);
+	bsp_delay_ms(3000);
+	display_render_logo();
+	display_scrollDiagRight(DISP_PAGE_0, DISP_PAGE_1);
+	bsp_delay_ms(3000);
+	display_render_logo();
+	display_scrollUp(SCROLL_BY_2FPS);
+	bsp_delay_ms(3000);
+	display_scrollUp(SCROLL_BY_3FPS);
+	bsp_delay_ms(3000);
+	display_scrollUp(SCROLL_BY_4FPS);
+	bsp_delay_ms(3000);
+	display_scrollUp(SCROLL_BY_5FPS);
+	bsp_delay_ms(3000);
+	display_scrollUp(SCROLL_BY_25FPS);
+	bsp_delay_ms(3000);
+	display_scrollUp(SCROLL_BY_64FPS);
+	bsp_delay_ms(3000);
+	display_scrollUp(SCROLL_BY_128FPS);
+	bsp_delay_ms(3000);
+	display_scrollUp(SCROLL_BY_256FPS);
+	bsp_delay_ms(3000);
+	display_render_logo();
+	display_scrollStop();
 }
 #endif
 /* Private functions ---------------------------------------------------------*/

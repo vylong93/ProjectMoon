a6a9c6e9687abd7d70dbe7f444d9218535719a03
Long Vy. Dang <vylong93khtn@gmail.com>
2017-03-23 21:47:59 +0700

    Text:add source files
    Signed-off-by: Long Vy. Dang <vylong93khtn@gmail.com>

---
+++ app/inc/text.h
@@ -0,0 +1,58 @@
+/**
+ ****************************************************************************
+ * @file        text.h
+ * @author      Long Dang
+ * @version     V0.1
+ * @date        23-March-2017
+ * @copyright   LGPLv3
+ * @brief       This is the header of the Text library.
+ ****************************************************************************
+ * @attention
+ *
+ * <h2><center>&trade; PnL - Programming and Leverage </center></h2>
+ *
+ * This file is part of Project Moon.
+ *
+ *   Project Moon is free embedded software: you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public License as
+ *   published by the Free Software Foundation, either version 3 of thescratch
+ *   License, or (at your option) any later version.
+ *
+ *   Project Moon is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *   See the GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with Project Moon.
+ *   If not, see <http://www.gnu.org/licenses>.
+ ****************************************************************************
+ */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef TEXT_H_
+#define TEXT_H_
+
+/** @defgroup LIB Library
+ * @{
+ */
+/** @defgroup LIB_TEXT Text
+ * @{
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "graphic.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions --------------------------------------------------------*/
+
+/**
+ * @} LIB_TEXT
+ */
+/**
+ * @} LIB
+ */
+#endif /* TEXT_H_ */
+
+/********************** (TM) PnL - Programming and Leverage ****END OF FILE****/

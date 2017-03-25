b5fe96293e21de9bdc82db75860217483d09346f
Long Vy. Dang <vylong93khtn@gmail.com>
2017-03-25 14:23:48 +0700

    SDIO:add source files
    Signed-off-by: Long Vy. Dang <vylong93khtn@gmail.com>

---
+++ bsp/inc/sd_io.h
@@ -0,0 +1,58 @@
+/**
+ ****************************************************************************
+ * @file        sd_io.h
+ * @author      Long Dang
+ * @version     V0.1
+ * @date        25-March-2017
+ * @copyright   LGPLv3
+ * @brief       This is the header of the SD I/O controler.
+ ****************************************************************************
+ * @attention
+ *
+ * <h2><center>&trade; PnL - Programming and Leverage </center></h2>
+ *
+ * This file is part of Project Moon.
+ *
+ *   Project Moon is free embedded software: you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public License as
+ *   published by the Free Software Foundation, either version 3 of thescratch
+ *   License, or (at your option) any later version.
+ *
+ *   Project Moon is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *   See the GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with Project Moon.
+ *   If not, see <http://www.gnu.org/licenses>.
+ ****************************************************************************
+ */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef SDIO_H_
+#define SDIO_H_
+
+/** @defgroup BSP Board Support Package
+ * @{
+ */
+/** @defgroup BSP_SDIO SD I/O
+ * @{
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_bsp.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions --------------------------------------------------------*/
+
+/**
+ * @} BSP_SDIO
+ */
+/**
+ * @} LIB
+ */
+#endif /* SDIO_H_ */
+
+/********************** (TM) PnL - Programming and Leverage ****END OF FILE****/
---
+++ bsp/src/sd_io.c
@@ -0,0 +1,49 @@
+/**
+ ****************************************************************************
+ * @file        sd_io.c
+ * @author      Long Dang
+ * @version     V0.1
+ * @date        25-March-2017
+ * @copyright   LGPLv3
+ * @brief       This file implement the SD I/O controller APIs.
+ ****************************************************************************
+ * @attention
+ *
+ * <h2><center>&trade; PnL - Programming and Leverage </center></h2>
+ *
+ * This file is part of Project Moon.
+ *
+ *   Project Moon is free embedded software: you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public License as
+ *   published by the Free Software Foundation, either version 3 of the
+ *   License, or (at your option) any later version.
+ *
+ *   Project Moon is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *   See the GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with Project Moon.
+ *   If not, see <http://www.gnu.org/licenses>.
+ ****************************************************************************
+ */
+/** @defgroup BSP_SDIO SD I/O
+ * @{
+ */
+/* Includes ------------------------------------------------------------------*/
+#include "sd_io.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private functions declaration ---------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+
+/* Exported functions prototype ----------------------------------------------*/
+
+/**
+ * @} BSP_SDIO
+ */
+/********************** (TM) PnL - Programming and Leverage ****END OF FILE****/

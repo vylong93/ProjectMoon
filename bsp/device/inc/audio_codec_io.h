ad084719ca74ec020c946aba39f7215808387f10
Long Vy. Dang <vylong93khtn@gmail.com>
2017-04-02 17:23:28 +0700

    ACODECIO:add source files Audio CODEC I/O Controller
    Signed-off-by: Long Vy. Dang <vylong93khtn@gmail.com>

---
+++ bsp/device/inc/audio_codec_io.h
@@ -0,0 +1,50 @@
+/**
+ ****************************************************************************
+ * @file        audio_codec_io.h
+ * @author      Long Dang
+ * @version     V0.1
+ * @date        02-April-2017
+ * @copyright   LGPLv3
+ * @brief       This is the header of the VS1003 device IO controller.
+ ****************************************************************************
+ * @attention
+ *
+ * <h2><center>&trade; PnL - Programming and Leverage </center></h2>
+ *
+ * This file is part of Project Moon.
+ *
+ *   Project Moon is free embedded software: you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public License as
+ *   published by the Free Software Foundation, either version 3 of the
+ *   License, or (at your option) any later version.
+ *
+ *   Project Moon is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *   See the GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with Project Moon.
+ *   If not, see <http://www.gnu.org/licenses>.
+ ****************************************************************************
+ */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef AUDIO_CODEC_IO_H_
+#define AUDIO_CODEC_IO_H_
+
+/** @addtogroup BSP_DEVICE_ACODEC
+ * @{
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_bsp.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions --------------------------------------------------------*/
+
+/**@}BSP_DEVICE_ACODEC*/
+#endif /* AUDIO_CODEC_IO_H_ */
+
+/********************** (TM) PnL - Programming and Leverage ****END OF FILE****/

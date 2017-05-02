9fb1044e4cee48c2704755a37eff960ffd9bce07
Long Vy. Dang <vylong93khtn@gmail.com>
2017-05-02 11:04:24 +0700

    Button:add template source files
    Signed-off-by: Long Vy. Dang <vylong93khtn@gmail.com>

---
+++ bsp/device/src/button.c
@@ -0,0 +1,51 @@
+/**
+ ****************************************************************************
+ * @file        button.c
+ * @author      Long Dang
+ * @version     V0.1
+ * @date        01-May-2017
+ * @copyright   LGPLv3
+ * @brief       This file implement the button IO handlers.
+ ****************************************************************************
+ * @attention
+ *
+ * <h2><center>&trade; PnL - Programming and Leverage </center></h2>
+ *
+ * This file is part of Project Moon.
+ *
+ *   Project Moon is free embedded software: you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public License as
+ *   published by the Free Software Foundation, either version 3 of the
+ *   License, or (at your option) any later version.
+ *
+ *   Project Moon is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *   See the GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with Project Moon.
+ *   If not, see <http://www.gnu.org/licenses>.
+ ****************************************************************************
+ */
+/** @addtogroup BSP_DEVICE_BUTTON
+ * @{
+ */
+/** @defgroup BSP_DEVICE_BUTTON_PRIVATE Button (Private)
+ * @{
+ */
+/* Includes ------------------------------------------------------------------*/
+#include "button.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private functions declaration ---------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+
+/* Exported functions prototype ----------------------------------------------*/
+
+/**@}BSP_DEVICE_BUTTON_PRIVATE*/
+/**@}BSP_DEVICE_BUTTON*/
+/********************** (TM) PnL - Programming and Leverage ****END OF FILE****/

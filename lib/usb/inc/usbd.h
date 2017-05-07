462d2a469c42d207cc429e33ca5fb1d36ddba4e3
Long Vy. Dang <vylong93khtn@gmail.com>
2017-05-07 14:58:54 +0700

    USBD:add USB Mass Storage Device source files
    Signed-off-by: Long Vy. Dang <vylong93khtn@gmail.com>

---
+++ lib/usb/inc/usbd.h
@@ -0,0 +1,50 @@
+/**
+ ****************************************************************************
+ * @file        usbd.h
+ * @author      Long Dang
+ * @version     V0.1
+ * @date        02-April-2017
+ * @copyright   LGPLv3
+ * @brief       This is the header of the USB Mass Storage Device module.
+ ****************************************************************************
+ * @attention
+ *
+ * <h2><center>&trade; PnL - Programming and Leverage </center></h2>
+ *
+ * This file is part of Project Moon.
+ *
+ *   Project Moon is free embedded software: you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public License as
+ *   published by the Free Software Foundation, either version 3 of the
+ *   License, or (at your option) any later version.
+ *
+ *   Project Moon is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *   See the GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with Project Moon.
+ *   If not, see <http://www.gnu.org/licenses>.
+ ****************************************************************************
+ */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef USBD_H_
+#define USBD_H_
+
+/** @addtogroup LIB_USBD
+ * @{
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_bsp.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions --------------------------------------------------------*/
+
+/**@}LIB_USBD*/
+#endif /* USBD_H_ */
+
+/********************** (TM) PnL - Programming and Leverage ****END OF FILE****/
